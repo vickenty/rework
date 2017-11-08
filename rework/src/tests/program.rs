f47376b2724280d37e4b8a42dfff33c77b669005
Vickenty Fesunov <kent@setattr.net>
2017-11-08 19:58:21 +0100

    Allow attributes on any item

--- rework/src/tests/program.rs
+++ rework/src/tests/program.rs
@@ -1,12 +1,16 @@
 parse! {
     program simple1 {
+        #[macro_use]
         use std::io;
         use std::collections::{HashMap, HashSet};
+
+        #[repr(C)]
         struct Set<K>(HashSet<K, Box<Inner>>);
         struct Inner {
             set: Set<::std::vec::Vec<u8>>,
             count: u8,
         }
+        #[derive(Debug)]
         enum Value {
             Empty,
             Single(Inner),
@@ -15,6 +19,7 @@ parse! {
                 second: Option<Inner>,
             }
         }
+        #[inline]
         pub fn test() -> u8 {
             let foo: u8 = MAX;
             foo
