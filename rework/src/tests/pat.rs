acede7c1d7dc12a32c58fc2a62b2a0016d720083
Vickenty Fesunov <kent@setattr.net>
2017-11-08 21:41:58 +0100

    Add splat syntax to patterns

--- rework/src/tests/pat.rs
+++ rework/src/tests/pat.rs
@@ -15,13 +15,18 @@ parse! {
     pattern tuple_single { (1,) }
     pattern tuple_simple { (1, 2) }
     pattern tuple_nested { ((1, 2), ref a, (), ) }
+    pattern tuple_splat_mid { (a, b, .., c) }
+    pattern tuple_splat_empty { (..) }
+
 
     pattern path { foo::Bar }
     pattern path_qual { <Type<Arg> as Trait<Arg>>::Foo::<Arg>::Bar }
 
     pattern struct_tuple1 { Some(1) }
     pattern struct_tuple2 { foo::Foo("bar", ref x) }
+    pattern struct_tuple_splat { Named(x, ..) }
     pattern struct_named1 { Point::<f32> { x, ref y } }
     pattern struct_named2 { ::th::BBox { tl, br: Point { x, y } } }
+    pattern struct_named_splat { S { foo, bar, .. } }
     pattern ref_pat { &(a, b) }
 }
