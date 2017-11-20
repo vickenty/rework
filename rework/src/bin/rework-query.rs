7e0e66859aadfa6a3cb44e1aabe9a5dd784c59c4
Vickenty Fesunov <kent@setattr.net>
2017-11-20 22:28:08 +0100

    Add experimental outline view

--- rework/src/bin/rework-query.rs
+++ rework/src/bin/rework-query.rs
@@ -8,6 +8,7 @@ use std::fs::File;
 use std::io::Read;
 use rework::parser;
 use rework::query_parser;
+use rework::syntax::Elem;
 use rework::query;
 use docopt::Docopt;
 
@@ -16,18 +17,20 @@ struct Args {
     arg_name: String,
     arg_query: String,
     flag_verbose: bool,
+    flag_outline: bool,
 }
 
 const USAGE: &str = r"
 Run a query over a file
 
 Usage:
-    rework-query [-v] <name> <query>
+    rework-query [-v] [-o] <name> <query>
     rework-query -h
 
 Options:
     -h --help     Show this message
     -v --verbose  Show verbose output
+    -o --outline  Show outline of each matched item
 ";
 
 fn main() {
@@ -40,6 +43,52 @@ fn main() {
     }
 }
 
+const PADDING: &str = "  ";
+
+fn outline(e: &Elem, depth: usize) {
+    if let Elem::Node { kind, ref children } = *e {
+        for _ in 0..depth {
+            print!("{}", PADDING);
+        }
+
+        print!("{}:", kind);
+
+        let mut seen = None;
+        let mut count = 0;
+
+        for child in children {
+            match *child {
+                Elem::Text { text, .. } => {
+                    print!(" \x1b[1m{}\x1b[0m", text);
+                    seen = None;
+                },
+                Elem::Node { kind, .. } => {
+                    if seen == Some(kind) {
+                        count += 1;
+                        if count == 2 {
+                            print!("\x1b[2m…\x1b[0m");
+                        }
+                    }
+                    else {
+                        print!(" \x1b[3m\x1b[2m{}\x1b[0m", kind);
+                        seen = Some(kind);
+                        count = 1;
+                    }
+                }
+            }
+        }
+
+        print!("\n");
+
+        for child in children {
+            if let Elem::Node { kind: "empty", .. } = *child {
+                continue
+            }
+            outline(child, depth + 1);
+        }
+    }
+}
+
 fn run(args: &Args) -> Result<(), Box<Error>> {
     let mut buffer = String::new();
     let mut file = File::open(&args.arg_name)?;
@@ -47,7 +96,9 @@ fn run(args: &Args) -> Result<(), Box<Error>> {
 
     let mut root = parser::module(&buffer, true)?;
     let query = query_parser::query(&args.arg_query)?;
-    query::find(&mut root, &query, &mut |e| if args.flag_verbose {
+    query::find(&mut root, &query, &mut |e| if args.flag_outline {
+        outline(e, 0);
+    } else if args.flag_verbose {
         println!("{:#?}", e);
     } else {
         println!("{}", e);
