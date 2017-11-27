126d83d4dc3e9fd076d46c4e02b09eaa4403c452
Vickenty Fesunov <kent@setattr.net>
2017-11-27 21:52:13 +0100

    Overhaul lists

--- rework/src/tests/type_name.rs
+++ rework/src/tests/type_name.rs
@@ -39,7 +39,7 @@ parse! {
 
 parse! {
     type_name tuple_unit { () } => { .. type_tuple empty }
-    type_name tuple_one { (u8,) } => { .. type_tuple list [= 1] }
-    type_name tuple_two { (u8, u8) } => { .. type_tuple list [= 2] }
+    type_name tuple_one { (u8,) } => { .. type_tuple tuple [= 1] }
+    type_name tuple_two { (u8, u8) } => { .. type_tuple tuple [= 2] }
     type_name grouped { (u8) } => { .. type_group }
 }
